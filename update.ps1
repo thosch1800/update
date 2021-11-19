5c1cafda3e0ed32aeaf9de364e13e25c0b2c2332
Thomas Schardt <30317420+thosch1800@users.noreply.github.com>
2021-11-19 15:54:46 +0100

    initial commit

---
+++ update.ps1
@@ -0,0 +1,10 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
+
+control update
+winget source update
+winget upgrade --all
